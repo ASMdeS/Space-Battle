ba36c50e31cb8a557336dd2b1dda917dbb7bea0c
asms <asms@cin.ufpe.br>
2023-09-11 19:10:17 -0300

    11/09 Troca de Grad

--- main.py
+++ main.py
@@ -2,24 +2,29 @@ import pygame
 from pygame.locals import *
 from sys import exit
 
+
 class Botao():
     # CONSTRUTOR DA CLASSE
-    def __init__(self, imagem, posx, posy): 
+    def __init__(self, imagem, posx, posy):
         self.x = posx
         self.y = posy
         self.image = pygame.image.load(imagem)
         self.rect = self.image.get_rect(center=(self.x, self.y))
+
     # COORDENADAS DO BOTÃO
     def coord(self):
         self.rect.topleft = self.x, self.y
+
     # CHECA SE HOUVE CLIQUE NA AREA DO BOTAO
     def apertado(self, mousx, mousy, top, bottom):
         if mousx in range(195, 405) and mousy in range(top, bottom):
             return True
         return False
+
     # MOSTRA O BOTÃO NA TELA
     def mostrar_botao(self, place):
         place.blit(self.image, (self.x, self.y))
+
 
 pygame.init()
 screen = pygame.display.set_mode((600, 640))
@@ -27,8 +32,46 @@ menu = pygame.image.load('images/menu.png')
 musica_de_fundo = pygame.mixer.music.load('sounds/CXR ATK - Dimensions.mp3')
 pygame.mixer.music.play(-1)
 
+# Player 1
+playerwhite_Img = pygame.image.load('images/nave_1.png')
+playerwhite_X = 284
+playerwhite_Y = 50
+playerwhiteX_change = 0
+playerwhiteY_change = 0
+
+
+def player_white(x, y):
+    screen.blit(playerwhite_Img, (x, y))
+
+
+# Player 2
+playerred_Img = pygame.image.load('images/nave_2.png')
+playerred_X = 284
+playerred_Y = 558
+playerredX_change = 0
+playerredY_change = 0
+
+
+def player_red(x, y):
+    screen.blit(playerred_Img, (x, y))
+
+
 # FUNÇÃO QUE MOSTRA O MENU PRINCIPAL
 def menu_principal():
+    # Player white
+    playerwhite_Img = pygame.image.load('images/nave_1.png')
+    playerwhite_X = 284
+    playerwhite_Y = 50
+    playerwhiteX_change = 0
+    playerwhiteY_change = 0
+
+    # Player red
+    playerred_Img = pygame.image.load('images/nave_2.png')
+    playerred_X = 284
+    playerred_Y = 558
+    playerredX_change = 0
+    playerredY_change = 0
+
     while True:
         pygame.display.set_caption('Menu Principal')
         # CRIAÇÃO DOS BOTÕES
@@ -38,7 +81,7 @@ def menu_principal():
         botao_sair.mostrar_botao(screen)
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:  
+            if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
             if event.type == MOUSEBUTTONDOWN:
@@ -46,12 +89,46 @@ def menu_principal():
                 if botao_jogar.apertado(mouse_x, mouse_y, 330, 415):
                     # RODAR O JOGO
                     print('jogar')
-                if botao_sair.apertado(mouse_x,mouse_y, 455, 540):
+                    jogar()
+                if botao_sair.apertado(mouse_x, mouse_y, 455, 540):
                     # FECHA A JANELA
                     print('sair')
                     pygame.quit()
                     exit()
         pygame.display.update()
         screen.blit(menu, (0, 0))
+
+
+def jogar():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    playerwhiteX_change = -5
+                if event.key == pygame.K_RIGHT:
+                    playerwhiteX_change = 5
+                #if event.key == pygame.K_SPACE:
+                    #if bullet_state == "ready":
+                        #bulletX = playerX
+                        #fire_bullet(bulletX, bulletY)
+            #if event.type == pygame.KEYUP:
+                #if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    playerX_change = 0
+        screen.fill((0, 0, 0))
+
+        playerwhite_X += playerwhiteX_change
+
+        if playerX < 0:
+            playerX = 0
+        elif playerX > 736:
+            playerX = 736
+
+        player_white(playerwhite_X, playerwhite_Y)
+        player_red(playerred_X, playerred_Y)
+        pygame.display.update()
+
 
 menu_principal()
